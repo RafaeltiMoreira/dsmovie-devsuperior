3cebd170b137e87a7ff29bec9cc80d9e89a2853e
Rafael Moreira <rafaeltimoreirass@gmail.com>
2022-01-14 22:33:25 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -4,16 +4,25 @@ import Pagination from "components/Pagination";
 //import MovieScore from "components/MovieScore";
 import MovieCard from "components/MovieCard";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
+import { useState, useEffect } from "react";
 
 function Listing() {
 
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-        .then(response => {
-            console.log(response.data);
-        });
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                console.log(data);
+                setPageNumber(data.number);
+            });
+    }, []);
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
